c80783ed22d895bf4e3613e1b14a94c6bf5f4001
Beck Larson - Work <becklarson-Work@jovys-mbp.attlocal.net>
2018-11-22 11:35:57 -0600

    adding the first version of v3

---
+++ website-v3/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+let port = 3000;
+app.listen(port, ()=> console.log('Portfolio King ready on port: ' + port));
+
+app.get('',function(req,resp,next){
+    resp.sendFile(__dirname + '/index.html');
+});
+app.use('/pages', express.static('pages'));
+app.use('/css', express.static('css'));
+app.use('/js', express.static('js'));
+app.use('/pictures', express.static('pictures'));
