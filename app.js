34376fa537269a609c13d67d6a1ffea363e64612
BeckLarson <40636995+BeckLarson@users.noreply.github.com>
2019-01-18 14:13:50 -0500

    portfolio changes

---
+++ app.js
@@ -0,0 +1,32 @@
+$(function () {
+    ChangePage("portfolio");
+    $('.nav-link').click(PageManager);
+});
+
+function PageManager(event) {
+    let page = event.target.innerText;
+    switch (page) {
+        case "About":
+            $('.portfolio').removeClass('active');
+            $('.about').addClass('active');
+            ChangePage(page);
+            break;
+        case "Portfolio":
+            $('.about').removeClass('active');
+            $('.portfolio').addClass('active');
+            ChangePage(page);
+            break;
+    }
+
+}
+
+function ChangePage(page) {
+    $.ajax({
+        type: "GET",
+        url: page + ".html",
+        dataType: "html",
+        success: function (msg) {
+            $('.display').html(msg);
+        }
+    });
+}
